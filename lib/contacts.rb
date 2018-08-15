40badef3abdc06dfc6e1620c2bc997416fe5116a
Alison Shin <alisonyshin@gmail.com>
2018-08-15 18:43:52 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -11,11 +11,11 @@ contacts = {
     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]}
   }
   
-def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    binding.pry
-    # if person == "Freddy Mercury"
-    #   details.each do { |attribute, values|
-    #   if attribute == "favorite_ice_cream_flavors"
-    #     values.
-end
+# def remove_strawberry(contacts)
+#   contacts.each do |person, contact_details_hash|
+#     binding.pry
+#     # if person == "Freddy Mercury"
+#     #   details.each do { |attribute, values|
+#     #   if attribute == "favorite_ice_cream_flavors"
+#     #     values.
+# end
